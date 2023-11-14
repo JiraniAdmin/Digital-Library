46c01fbbc40ecf11c0208552cc523773b7985ac3
bismansahni <bismansahni@outlook.com>
2023-11-13 18:40:16 -0700

    updated the search capability

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import sqlite3
 
-def connect_to_db(db_file):
-    """Connect to the SQLite database."""
-    conn = sqlite3.connect(db_file)
-    return conn
 
 def search_books(conn, title=None, author=None):
     """Search for books by title or author."""
+    """Connect to the SQLite database."""
+    conn = sqlite3.connect("/home/jirani/library.db")
+  
+
     cur = conn.cursor()
+
     
     if title:
         cur.execute("SELECT * FROM books WHERE title LIKE ?", ('%' + title + '%',))
@@ -21,21 +22,23 @@ def search_books(conn, title=None, author=None):
     for book in books:
         print(book)
 
-def main():
-    database = "your_database.db"  # Replace with your SQLite database file
+# def main():
+#     database = "your_database.db"  # Replace with your SQLite database file
     
-    # Connect to the database
-    conn = connect_to_db(database)
+  
+   
 
-    # Example search
-    search_input = input("Enter a book title or author's name: ")
-    if " " in search_input:  # Assuming author names have spaces and titles may not
-        search_books(conn, author=search_input)
-    else:
-        search_books(conn, title=search_input)
+#     # Example search
+#     search_input = input("Enter a book title or author's name: ")
+#     if " " in search_input:  # Assuming author names have spaces and titles may not
+#         search_books(conn, author=search_input)
+#     else:
+#         search_books(conn, title=search_input)
 
-    # Close the connection
-    conn.close()
+#     # Close the connection
+#     conn.close()
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+search_books("Lorax", author=None)
