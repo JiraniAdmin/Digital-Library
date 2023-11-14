e3ac695dcb9b0e8e91943bafd036fa1f74aa8c57
ananthnarne <57265147+ananthnarne@users.noreply.github.com>
2023-11-13 18:22:20 -0700

    Update db_commands.py

--- src/db_commands.py
+++ src/db_commands.py
@@ -1,13 +1,13 @@
 import sqlite3
 
 
-def add_book(title, author):
+def add_book(title, author, path, description):
     # Add a new book
     # Connect to SQLite database
     conn = sqlite3.connect('/home/jirani/library.db')
     cursor = conn.cursor()
 
-    cursor.execute('INSERT INTO books (title, author) VALUES (?, ?)', (title, author))
+    cursor.execute('INSERT INTO books (title, author, path, description) VALUES (?, ?)', (title, author, path, description))
     
     # Retrieve the new count
     # cursor.execute('SELECT book_count FROM counters WHERE id = 1')
@@ -16,9 +16,5 @@ def add_book(title, author):
 
     conn.close()
 
-# Example usage
-add_book("1984", "George Orwell")
-add_book("To Kill a Mockingbird", "Harper Lee")
+add_book("Lorax", "Dr. Suess", "/home/jirani", "Children's Book")
 
-# Close the connection
-conn.close()
