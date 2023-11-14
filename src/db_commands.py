1997a34f31016c3f6701ce73b32835399f8b64ff
AstronomicalWind <91765427+AstronomicalWind@users.noreply.github.com>
2023-11-13 17:56:13 -0700

    added try catch

--- src/db_commands.py
+++ src/db_commands.py
@@ -22,32 +22,38 @@ def create_library_database():
     Returns:
         bool: true if created, false if not
     """
-    # Connect to SQLite database (or create a new one if it doesn't exist)
-    db_path = "/home/jirani/library.db"
-    print(db_path)
-    conn = sqlite3.connect(db_path)
+    try:
+        # Connect to SQLite database (or create a new one if it doesn't exist)
+        db_path = "/home/jirani/library.db"
+        print(db_path)
+        conn = sqlite3.connect(db_path)
 
-    # Create a cursor object to interact with the database
-    cursor = conn.cursor()
+        # Create a cursor object to interact with the database
+        cursor = conn.cursor()
 
-    # Create a table with columns "Title", "Author", "Path", and "Description"
-    cursor.execute('''
+        # Create a table with columns "Title", "Author", "Path", and "Description"
+        cursor.execute('''
         CREATE TABLE IF NOT EXISTS books (
             Title TEXT,
             Author TEXT,
             Path TEXT,
             Description TEXT
         )
-    ''')
+        ''')
 
-    # Commit the changes and close the database connection
-    conn.commit()
-    conn.close()
+        # Commit the changes and close the database connection
+        conn.commit()
+        conn.close()
 
-    print("Author, Path, and Description columns created.")
-
-
-
-create_library_database()
+        print("Author, Path, and Description columns created.")
+        return True
+    
+    except sqlite3.Error as err:
+        print(f"SQLite error: {err}")
+        return False
+        
+    except Exception as err:
+        print(f"Error occurred: {err}")
+        return False
 
           
