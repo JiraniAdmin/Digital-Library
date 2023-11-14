6ace5701b076671c9a168be0f95f6fc63402c43b
ananthnarne <57265147+ananthnarne@users.noreply.github.com>
2023-11-13 18:19:06 -0700

    Update db_commands.py

--- src/db_commands.py
+++ src/db_commands.py
@@ -1,41 +1,20 @@
 import sqlite3
 
-# Connect to SQLite database
-conn = sqlite3.connect('library.db')
-cursor = conn.cursor()
-
-# Create tables if they don't exist
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS books (
-    id INTEGER PRIMARY KEY,
-    title TEXT,
-    author TEXT
-)
-''')
-
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS counters (
-    id INTEGER PRIMARY KEY CHECK (id = 1),
-    book_count INTEGER DEFAULT 0
-)
-''')
-
-# Initialize counter
-cursor.execute('INSERT OR IGNORE INTO counters (id, book_count) VALUES (1, 0)')
-conn.commit()
 
 def add_book(title, author):
     # Add a new book
+    # Connect to SQLite database
+    conn = sqlite3.connect('/home/jirani/library.db')
+    cursor = conn.cursor()
+
     cursor.execute('INSERT INTO books (title, author) VALUES (?, ?)', (title, author))
     
-    # Increment the counter
-    cursor.execute('UPDATE counters SET book_count = book_count + 1 WHERE id = 1')
-    conn.commit()
-
     # Retrieve the new count
-    cursor.execute('SELECT book_count FROM counters WHERE id = 1')
-    count = cursor.fetchone()[0]
-    print(f"Book added. Total books in database: {count}")
+    # cursor.execute('SELECT book_count FROM counters WHERE id = 1')
+    # count = cursor.fetchone()[0]
+    print(f"Book {title} added.")
+
+    conn.close()
 
 # Example usage
 add_book("1984", "George Orwell")
